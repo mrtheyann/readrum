c681248527495629c7c68b64c4ca1ad62fdd9fc9
Yann Pavlenko <mrtheyann@gmail.com>
2020-01-16 22:11:05 +0500

    Updates

---
+++ precache-manifest.92d5457fd8b6e77451969a4c03064488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ea610c26efe5bc2f734b0f8fca5670",
+    "url": "/readrum/index.html"
+  },
+  {
+    "revision": "ee877e3fd6b130500069",
+    "url": "/readrum/static/css/main.88091997.chunk.css"
+  },
+  {
+    "revision": "c244b2bf5fbedcccf6e7",
+    "url": "/readrum/static/js/2.63d52cea.chunk.js"
+  },
+  {
+    "revision": "1d0685af7ecadd53a914fca680b87872",
+    "url": "/readrum/static/js/2.63d52cea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee877e3fd6b130500069",
+    "url": "/readrum/static/js/main.94d734be.chunk.js"
+  },
+  {
+    "revision": "59b019136e607d07a9b9",
+    "url": "/readrum/static/js/runtime-main.a2588bd6.js"
+  }
+]);
